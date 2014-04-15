42e13f5935abce5851a4a63f94a73d33125e1c3d
sewald01 <sewald01@seattlecentral.edu>
2014-04-15 14:12:07 -0700

    Assignment 1
    Assignment 1

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,3 +1,20 @@
-<!doctype html>
-<html>
-Hello cruel world!
+<?php
+$page_title = "Index page";
+include("head.inc");
+?>
+
+This space intentionally left blank.
+
+<?php
+//Commenting out php debug information:
+//phpinfo();
+
+
+?>
+
+This space also intentionally left blank.
+
+<?php
+$page_title = "Index page";
+include("foot.inc");
+?>
